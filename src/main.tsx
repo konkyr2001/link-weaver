492a3c32bb969db6ea7b381b2177f73e852e32b0
konkyr2001 <konkyr2001@hotmail.com>
2026-04-18 20:54:28 +0300

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Analytics } from "@vercel/analytics/react"
 createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider 
     clientId={import.meta.env.VITE_GOOGLE_API_CLIENT_ID}
     locale="en"
     >
     <App />
+    <Analytics />
   </GoogleOAuthProvider>,
 );
