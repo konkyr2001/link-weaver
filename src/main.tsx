b5192c8df51ada3404593710655b050aba8f8edf
konkyr2001 <konkyr2001@hotmail.com>
2026-04-18 21:25:54 +0300

    vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider 
     clientId={import.meta.env.VITE_GOOGLE_API_CLIENT_ID}
     locale="en"
     >
-    <App />
     <Analytics />
+    <SpeedInsights />
+    <App />
   </GoogleOAuthProvider>,
 );
  
